1bcc4a5076150f8c1ce5ac789444d849b6e6b879
l-k-11235 <linakhodja@gmail.com>
2024-06-17 17:50:36 +0200

    resized the key_pad_mask

--- eole/modules/multi_headed_attn.py
+++ eole/modules/multi_headed_attn.py
@@ -1,4 +1,5 @@
 """ Multi-Head Attention module """
+
 import torch
 import torch.nn as nn
 from math import log, sqrt
@@ -612,7 +613,7 @@ class SelfMHA(MultiHeadedAttention):
                 if step == 0:
                     key_pad_mask = self.layer_cache[1].get("key_pad_mask", None)
                     if key_pad_mask is not None:
-                        x = key_pad_mask.expand(-1, self.heads // self.parallel_gpu, -1)
+                        x = key_pad_mask.expand(-1, value.size(1), -1)
                         x = x.unsqueeze(3)
                         x = x.expand(-1, -1, -1, value.size(3))
                         value = value.masked_fill(x, 0)
