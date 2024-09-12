b7185d63c3191dba56883abab809d20e93cdbbf4
vince62s <vince62s@yahoo.com>
2024-09-12 16:13:13 +0200

    flake

--- recipes/wmt22_with_TowerInstruct-Mistral/promptize_mistral.py
+++ recipes/wmt22_with_TowerInstruct-Mistral/promptize_mistral.py
@@ -10,6 +10,8 @@ with open(input_file_path, "r") as input_file, open(
     for line in input_file:
         # Set up the prompt provided by Tower-instruct with the current line's content
         # PLEASE note the space between "<|im_start|> user" and "<|im_start|> assistant"
-        # This is due to a specific behavior of onmt_tokenize - HF tokenizer has a similar strange behavior: https://github.com/huggingface/transformers/issues/31513#issuecomment-2340151976
-        prompt = f"<|im_start|> user｟newline｠Translate the following text from English into German.｟newline｠English: {line.strip()}｟newline｠German:<|im_end|> ｟newline｠<|im_start|> assistant｟newline｠"
+        # This is due to a specific behavior of onmt_tokenize
+        # HF tokenizer has a similar strange behavior
+        # https://github.com/huggingface/transformers/issues/31513#issuecomment-2340151976
+        prompt = f"<|im_start|> user｟newline｠Translate the following text from English into German.｟newline｠English: {line.strip()}｟newline｠German:<|im_end|> ｟newline｠<|im_start|> assistant｟newline｠" # noqa: E501
         output_file.write(prompt + "\n")
