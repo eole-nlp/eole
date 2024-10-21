bffb710c3d9d4288bba1a747fbb14a3bf46e4970
fh <francois.hernandez.fh@gmail.com>
2024-10-21 21:05:43 +0200

    misc fixes, add wmt17 bpe/spm configs

--- eole/bin/run/build_vocab.py
+++ eole/bin/run/build_vocab.py
@@ -123,7 +123,7 @@ def build_vocab(config, transforms, n_sample=3):
         for c_name in corpora.keys()
     }
     if config.dump_samples:
-        sample_path = os.path.join(os.path.dirname(config.save_data), CorpusName.SAMPLE)
+        sample_path = os.path.join(config.save_data, CorpusName.SAMPLE)
         write_process = mp.Process(
             target=write_files_from_queues, args=(sample_path, queues), daemon=True
         )
