2701a88ee7fd817c89b6937d90d4c912619ef688
vince62s <vince62s@yahoo.com>
2025-03-27 21:13:06 +0100

    test

--- eole/config/inference.py
+++ eole/config/inference.py
@@ -108,7 +108,7 @@ class InferenceConfig(RunningConfig, DecodingConfig, LoRaConfig, QuantizeConfig)
     chat_template: str | None = None
     optional_eos: List[str] | None = Field(
         default=[],
-        description="Optional EOS tokens that would stop generation, e.g. <|eot_id|> for Llama3",
+        description="Optional EOS tokens that would stop generation",
     )
 
     def get_model_path(self):
