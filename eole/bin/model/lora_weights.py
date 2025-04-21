741ebed51a01138adc71dc60c60ad6f0bb2737ee
vince62s <vince62s@yahoo.com>
2025-04-21 09:14:47 +0200

    minor fix

--- eole/bin/model/lora_weights.py
+++ eole/bin/model/lora_weights.py
@@ -86,7 +86,10 @@ class LoraWeights(BaseBin):
                     if args.action == "merge":
                         lora_A = lora_dict.get_tensor(module_name + ".lora_A").transpose(0, 1)
                         lora_B = lora_dict.get_tensor(module_name + ".lora_B").transpose(0, 1)
-                        shard_dict[key] += (lora_A @ lora_B).transpose(0, 1) * scaling
+                        if 'embeddings' in key:
+                            shard_dict[key] += (lora_A @ lora_B) * scaling
+                        else:
+                            shard_dict[key] += (lora_A @ lora_B).transpose(0, 1) * scaling
                     elif args.action == "concat":
                         extra_dict[module_name + ".lora_A"] = lora_dict.get_tensor(module_name + ".lora_A")
                         extra_dict[module_name + ".lora_B"] = lora_dict.get_tensor(module_name + ".lora_B")
