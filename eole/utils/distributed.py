a523b1b7292fd8bc27c6caceaa10c9e7251da1f7
vince62s <vince62s@yahoo.com>
2026-01-04 19:18:11 +0100

    .

--- eole/utils/distributed.py
+++ eole/utils/distributed.py
@@ -107,8 +107,9 @@ def all_gather_list(data: Any, max_size: int = DEFAULT_MAX_GATHER_SIZE) -> List[
     # Encode size using 2 bytes (supports up to ~65k)
     in_buffer[0] = enc_size // ENCODING_BASE
     in_buffer[1] = enc_size % ENCODING_BASE
-    enc_tensor = torch.frombuffer(enc, dtype=torch.uint8).to(in_buffer.device)
-    in_buffer[2 : enc_size + 2] = enc_tensor
+    in_buffer[2 : enc_size + 2].copy_(
+        torch.frombuffer(bytearray(enc), dtype=torch.uint8)
+    )
 
     # All-gather buffers
     torch.distributed.all_gather(out_buffers, in_buffer)
