106b4f41af1afe5dfc1d795780683f3ef73baef1
vince62s <vince62s@yahoo.com>
2026-01-14 18:56:34 +0100

    refac setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 from os import path
-import torch
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 
-def get_cuda_arch_flags():
+def get_cuda_arch_flags(torch):
     flags = []
-
     try:
         if torch.cuda.is_available():
             major, minor = torch.cuda.get_device_capability()
@@ -29,73 +26,71 @@ def get_cuda_arch_flags():
     ]
 
 
-cuda_arch_flags = get_cuda_arch_flags()
+if __name__ == "__main__":
+    import torch
+    from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-setup(
-    name="eole",
-    description="Open language modeling toolkit based on PyTorch",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    version="0.4.4",
-    packages=find_packages(),
-    ext_modules=[
-        CUDAExtension(
-            name="eole.ops",  # This will be accessible as eole.ops
-            sources=[
-                "csrc/rms_norm.cu",
-                "csrc/rotary_embedding.cu",
-                "csrc/activation_kernels.cu",
-                "csrc/bindings.cpp",
-            ],
-            extra_compile_args={
-                "cxx": ["-O3", "-std=c++17"],
-                "nvcc": [
-                    "-O3",
-                    "--use_fast_math",
-                    "-std=c++17",
-                    "--expt-relaxed-constexpr",
-                    "--expt-extended-lambda",
-                ]
-                + cuda_arch_flags,
-            },
-        )
-    ],
-    cmdclass={"build_ext": BuildExtension},
-    project_urls={
-        "Source": "https://github.com/eole-nlp/eole/",
-    },
-    python_requires=">=3.10",
-    install_requires=[
-        "configargparse",
-        "ctranslate2>=4,<5",
-        "fastapi",
-        "fasttext-wheel",
-        "huggingface_hub",
-        "datasets",
-        "numpy>=2.0",
-        "pandas",
-        "protobuf==3.20.1",
-        "pyahocorasick",
-        "pyonmttok>=1.38.1,<2",
-        "pyyaml",
-        "rapidfuzz",
-        "rich",
-        "sacrebleu",
-        "safetensors",
-        "sentencepiece>=0.1.94,<0.1.98",
-        "six",
-        "spacy",
-        "subword-nmt>=0.3.7",
-        "tensorboard>=2.18.0",
-        "torch>=2.8,<2.10",
-        "torch-optimi",
-        "uvicorn",
-        "waitress",
-        "pydantic",
-    ],
-    entry_points={
-        "console_scripts": [
-            "eole=eole.bin.main:main",
+    cuda_arch_flags = get_cuda_arch_flags(torch)
+
+    setup(
+        name="eole",
+        description="Open language modeling toolkit based on PyTorch",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        version="0.4.4",
+        packages=find_packages(),
+        ext_modules=[
+            CUDAExtension(
+                name="eole.ops",  # This will be accessible as eole.ops
+                sources=[
+                    "csrc/rms_norm.cu",
+                    "csrc/rotary_embedding.cu",
+                    "csrc/activation_kernels.cu",
+                    "csrc/bindings.cpp",
+                ],
+                extra_compile_args={
+                    "cxx": ["-O3", "-std=c++17"],
+                    "nvcc": [
+                        "-O3",
+                        "--use_fast_math",
+                        "-std=c++17",
+                        "--expt-relaxed-constexpr",
+                        "--expt-extended-lambda",
+                    ] + cuda_arch_flags,
+                },
+            )
         ],
-    },
-)
+        cmdclass={"build_ext": BuildExtension},
+        project_urls={"Source": "https://github.com/eole-nlp/eole/"},
+        python_requires=">=3.10",
+        install_requires=[
+            "configargparse",
+            "ctranslate2>=4,<5",
+            "fastapi",
+            "fasttext-wheel",
+            "huggingface_hub",
+            "datasets",
+            "numpy>=2.0",
+            "pandas",
+            "protobuf==3.20.1",
+            "pyahocorasick",
+            "pyonmttok>=1.38.1,<2",
+            "pyyaml",
+            "rapidfuzz",
+            "rich",
+            "sacrebleu",
+            "safetensors",
+            "sentencepiece>=0.1.94,<0.1.98",
+            "six",
+            "spacy",
+            "subword-nmt>=0.3.7",
+            "tensorboard>=2.18.0",
+            "torch>=2.8,<2.10",
+            "torch-optimi",
+            "uvicorn",
+            "waitress",
+            "pydantic",
+        ],
+        entry_points={"console_scripts": ["eole=eole.bin.main:main"]},
+    )
+
